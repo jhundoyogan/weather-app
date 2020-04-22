a2a48221f79e40c3f4aaa2bec1a7447cea000777
Jhun Doyogan <doyoganjhunnester@gmail.com>
2020-04-22 18:48:47 +0800

    added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees Fahrenheit. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, 
+                     body.current.weather_descriptions[0] + 
+                     '. It is currently ' + body.current.temperature + ' degrees Fahrenheit. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
